88d89f169add433571f81520dc8cee0670bd3f14
ThiagoAlmeidaSiqueira <thiagoalmeidasiqueira@gmail.com>
2020-08-15 00:57:39 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 let Distancia = 0
+magicbit.MotorRun(magicbit.Motors.M1, 0)
+magicbit.GeekServo(magicbit.Servos.S1, 50)
 basic.forever(function () {
     Distancia = sonar.ping(
     DigitalPin.P1,
@@ -8,11 +10,11 @@ basic.forever(function () {
     if (Distancia <= 5 && Distancia > 0) {
         magicbit.MotorRun(magicbit.Motors.M1, 0)
         magicbit.GeekServo(magicbit.Servos.S1, 100)
-        magicbit.MotorRun(magicbit.Motors.M1, -128)
+        magicbit.MotorRun(magicbit.Motors.M1, -255)
         basic.pause(3000)
         magicbit.GeekServo(magicbit.Servos.S1, 50)
         magicbit.MotorRun(magicbit.Motors.M1, 0)
     } else {
-        magicbit.MotorRun(magicbit.Motors.M1, 128)
+        magicbit.MotorRun(magicbit.Motors.M1, 255)
     }
 })
